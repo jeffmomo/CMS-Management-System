aed7b45e5881dba5bccf3a4ea4b09d6b10689a2b
Jeff Mo <jeff941027@gmail.com>
2015-11-10 18:20:11 +0800

    Removed unnecessary components. ##UNTESTED##

--- src/main/java/com/seveneleven/Integrater.java
+++ src/main/java/com/seveneleven/Integrater.java
@@ -15,7 +15,7 @@ import java.sql.SQLException;
 /**
  * Created by mdl94 on 12/10/2015.
  */
-public class IntegrationTester {
+public class Integrater {
 
     public static void main(String[] args)
     {
